0eec0f0fe9cb2326a3f84c89ad77c1d03d030475
Mohit Dhaker <mohit.dhaker04@gmail.com>
2022-11-13 23:20:30 +0530

    login_function

--- bot.py
+++ bot.py
@@ -4,42 +4,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
+import time
 os.environ['PATH'] += r"C:/ChromeDriver"
 driver = webdriver.Chrome()
 
-my_user_name = "standard_user"
-my_password = "secret_sauce"
+my_email_id = "mohit.laxminarayan2021@vitbhopal.ac.in"
+my_password = "Mohit@461981"
 
 # Opens the URL page
-driver.get("https://www.saucedemo.com/")
+driver.get("https://teams.microsoft.com/")
 
-driver.implicitly_wait(5)  # wait for 3 sec after loading browser
+# driver.implicitly_wait(3)  # wait for 3 sec after loading browser
 
-try:
-    user_name = driver.find_element(By.ID, "user-name")
-    password = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.ID, "login-button")
-    addt_to_cart = driver.find_element(
-        By.ID, "add-to-cart-sauce-labs-bike-light")
-except:
-    print("No elements with this Id name")
 
-# send keys for sending information
-user_name.send_keys(my_user_name)
-password.send_keys(my_password)
+def login():
 
-try:
-    WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located((By.ID, "user-name"), "standard_user ")
-    )
-finally:
-    login_button.click()
+    global driver
+    # Enter Eamil ID
+    time.sleep(2)
+    email_field = driver.find_element(By.ID, "i0116")
+    email_field.send_keys(my_email_id)
+    driver.find_element(By.ID, "idSIButton9").click()
+    time.sleep(3)
+    # Enter Password
+    password_field = driver.find_element(By.ID, "i0118")
+    password_field.send_keys(my_password)
+    driver.find_element(By.ID, "idSIButton9").click()
+    time.sleep(3)
+    driver.find_element(By.ID, "idSIButton9").click()
 
-try:
-    WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located(
-            (By.ID, "add-to-cart-sauce-labs-bike-light"))
-    )
-finally:
-    addt_to_cart.click()
+
+login()
